66dca74812e751bfbc248f08381903046f270c84
Ustin4 <Mortyvp@yandex.by>
2023-10-11 21:27:10 +0300

    portfolio skills

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import style from './Skills.module.scss'
-import styleContainer from '../common/styles/Container.module.css'
 import Skill from "./skill/Skill";
 import Title from "../common/components/title/Title";
+import cssIcon from "../assets/image/icons8-css-logo-480.png";
+import jsIcon from "../assets/image/icons8-js-100.png"
+import reactIcon from "../assets/image/icons8-react-480.png"
 
 const Skills = () => {
+    const css = {
+        backgroundImage: `url(${cssIcon})`
+    };
+    const js = {
+        backgroundImage: `url(${jsIcon})`
+    };
+    const react = {
+        backgroundImage: `url(${reactIcon})`
+    };
     return (
         <div className={style.skillsBlock}>
-            <div className={`${styleContainer.container} ${style.skillsContainer}`}>
+            <div className={style.skillsContainer}>
                 <Title text={'Skills'}/>
                 <div className={style.skills}>
-                    <Skill title={'Js'} description={'123ewewda'}/>
-                    <Skill title={'CSS'} description={'sdasdasd'}/>
-                    <Skill title={'React'} description={'sadasdasd'}/>
+                    <Skill style={js} title={'Js'} description={'123ewewda'}/>
+                    <Skill style={css} title={'CSS'} description={'sdasdasd'}/>
+                    <Skill style={react} title={'React'} description={'sadasdasd'}/>
                 </div>
             </div>
         </div>
--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import style from './Skill.module.css'
+import style from './Skill.module.scss'
 
 type SkillPropsType = {
     title: string
     description: string
+    style:any
 }
 
 const Skill = (props: SkillPropsType) => {
     return (
         <div className={style.skill}>
-            <div className={style.icon}></div>
+            <div className={style.icon} style={props.style}></div>
             <h3>{props.title}</h3>
             <span className={style.description}>{props.description}</span>
         </div>
