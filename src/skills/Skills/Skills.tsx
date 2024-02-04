f0168185b1d5435869697d7dae2f7ccd9dfaed12
Ustin4 <Mortyvp@yandex.by>
2024-02-04 21:35:11 +0300

    add card and todolists

--- src/skills/Skills/Skills.tsx
+++ src/skills/Skills/Skills.tsx
@@ -1,30 +1,3 @@
-// import React from 'react';
-// import style from './Skills.module.scss'
-// import Skill from "./skill/Skill";
-// import Title from "../common/components/title/Title";
-// import cssIcon from "../assets/image/icons8-css-logo-480.png";
-// import jsIcon from "../assets/image/icons8-js-100.png"
-// import reactIcon from "../assets/image/icons8-react-480.png"
-//
-// const Skills = () => {
-//
-//     return (
-//         <div id='skills' className={style.skillsBlock}>
-//             <div className={style.skillsContainer}>
-//                 <Title text={'Skills'}/>
-//                 <div className={style.skills}>
-//                     <Skill title={'Js'} description={'This is a language from programin'} icon={jsIcon}/>
-//                     <Skill title={'CSS'} description={'sdasdasd'} icon={cssIcon}/>
-//                     <Skill title={'React'} description={'sadasdasd'} icon={reactIcon}/>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
-//
-// export default Skills;
-
-
 import style from './Skills.module.scss'
 
 import React from "react";
@@ -58,7 +31,7 @@ export const Skills = () => {
                 </li>
                 <li>
                     <span
-                        className={style.title}>API - Axios, Rest API, WebSocket, Socket.io, FireBase</span>
+                        className={style.title}>API - Axios, Rest API, WebSocket, Socket.io, FireBase,RTK Query</span>
                 </li>
                 <li>
                     <span
@@ -66,7 +39,7 @@ export const Skills = () => {
                 </li>
                 <li>
                     <span
-                        className={style.title}>Material Ul, Ant design</span>
+                        className={style.title}>Material Ul, Ant design, Radix ui</span>
                 </li>
                 <li>
                     <span className={style.title}>CSS preprocessors - SASS</span>
